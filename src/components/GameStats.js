6e1608e4652abc6773ef99a7fbe50628b875116b
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2021-11-26 12:55:06 +0000

    Little UI hover effect

--- src/components/GameStats.js
+++ src/components/GameStats.js
@@ -5,9 +5,9 @@ const GameStats = ({ games }) => {
     const [stats, setStats] = useState(undefined);
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
-    // useEffect(() => {
-    //     console.log(games)
-    // }, [games])
+    const [hovered, setHovered] = useState(undefined);
+
+    
 
     useEffect(() => {
         setLoading(true);
@@ -46,8 +46,9 @@ const GameStats = ({ games }) => {
     }
 
 
-    const segmentClick = (e) => {
-        console.log(e.target.value)
+    const segmentClick = (e, index) => {
+        console.log('Segment:', stats[Object.keys(stats)[index]]);
+
     }
 
     useEffect(() => {
@@ -56,36 +57,48 @@ const GameStats = ({ games }) => {
                 {
                     title: '< 1hr',
                     value: stats.LessOne.length,
-                    color: '#AFFF33',
+                    color: hovered === 0 ? 'grey' : '#AFFF33',
                 },
                 {
                     title: '1hr - 5hr',
                     value: stats.OneToFive.length,
-                    color: '#D9FF33',
+                    color: hovered === 1 ? 'grey' : '#D9FF33',
                 },
                 {
                     title: '5hr - 20hr',
                     value: stats.FiveToTwenty.length,
-                    color: '#F8FF33',
+                    color: hovered === 2 ? 'grey' : '#F8FF33',
                 },
                 {
                     title: '20hr - 100hr',
                     value: stats.TwentyToHundred.length,
-                    color: '#FFC933',
+                    color: hovered === 3 ? 'grey' : '#FFC933',
                 },
                 {
                     title: '100hr - 500hr',
                     value: stats.HundredToFiveHundred.length,
-                    color: '#FFA233',
+                    color: hovered === 4 ? 'grey' : '#FFA233',
                 },
                 {
                     title: '> 500hr',
                     value: stats.FiveHundredPlus.length,
-                    color: '#FF6933',
+                    color: hovered === 5 ? 'grey' : '#FF6933',
                 },
             ]);
         }
-    }, [stats])
+    }, [stats, hovered])
+
+    // useEffect(() => {
+    //     setData(data.map((entry, i) => {
+    //         if (hovered === i) {
+    //           return {
+    //             ...entry,
+    //             color: 'grey',
+    //           };
+    //         }
+    //         return entry;
+    //       }));
+    // }, [hovered])
 
     const conditionalRender = () => {
         if (loading) {
@@ -108,25 +121,35 @@ const GameStats = ({ games }) => {
                             fontSize: '0.25rem',
                             fill: 'rgb(165, 40, 127)',
                             textShadow: '1px 0.5px 2.5px rgb(252, 252, 253)',
-                            fontWeight: 'bold'
+                            fontWeight: 'bold',
+
+                        }}
+                        segmentsStyle={{
+                            transition: 'stroke .3s',
                         }}
                         labelPosition={90}
                         data={data}
                         animate
                         onClick={(event, index) => {
-                            console.log('Segment:', stats[Object.keys(stats)[index]]);
-                          }}
+                            segmentClick(event, index);
+                        }}
+                        onMouseOver={(_, index) => {
+                            setHovered(index);
+                        }}
+                        onMouseOut={() => {
+                            setHovered(undefined);
+                        }}
                     />
                     <br />
                     <div className="Pie-Chart-Key">
                         {data.map(d => {
                             return (
-                            <div 
-                                className="Pie-Chart-Key-Entry"
-                                style={{background: d.color}}
-                            >
-                                {d.title}
-                            </div>
+                                <div
+                                    className="Pie-Chart-Key-Entry"
+                                    style={{ background: d.color }}
+                                >
+                                    {d.title}
+                                </div>
                             )
                         })}
                     </div>
